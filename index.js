673ce7dded256f3608c7cbbb23f7be2335d13440
banelmikhael <banel.mikhael.bm@gmail.com>
2022-10-24 21:35:47 +0300

    assigment completed

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution here
+function findMatching(array,name){
+
+   const result=array.filter(item => item.toUpperCase()==name.toUpperCase())
+
+   if(result !== undefined){
+ 
+    return result
+   } else{
+    undefined
+   }
+}
+
+function fuzzyMatch(array,letter){
+    
+   //  const result=array.filter(name => name.charAt(0).toUpperCase()===letter.toUpperCase())
+   const result=array.filter(name => name.startsWith(letter));
+    return result
+}
+
+function matchName(array,name){
+    const result =array.filter(item => item.name ===name)
+
+    return result
+}
